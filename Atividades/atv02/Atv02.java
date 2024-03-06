5bca6c8995148c1ce84b2f1c2850f146477c599c
Raul <raulgallertseverino@gmail.com>
2024-03-06 09:21:34 -0300

    Atividade3

---
+++ Atividades/atv02/Atv02.java
@@ -0,0 +1,28 @@
+public class Atv02 {
+    public static void main(String[] args) {
+        int a = 2147483647;
+        long b = 543758998453484L;
+        float c = 5.75f;
+        double d = 10.45;
+        boolean e = true;
+        byte f = 127;
+        short g = 32767;
+        char h = 'c';
+        System.out.println("int: "+a);
+        System.out.println("long: "+b);
+        System.out.println("float: "+c);
+        System.out.println("double: "+d);
+        System.out.println("bolean: "+e);
+        System.out.println("byte: "+f);
+        System.out.println("short: "+g);
+        System.out.println("char: "+h);
+        String oi = "tudo bem?\n";
+        String sim = "como\t vai";
+        String agns = "aa\"foca\"aaa";
+        String afs = "ola\\r";
+        System.out.println("Pula linha: " +oi);
+        System.out.println("Tabulação: " +sim);
+        System.out.println("Aspas: " +agns);
+        System.out.println("Barra invertida: " +afs);
+    }
+}
---
+++ Atividades/atv02/atv03.java
@@ -0,0 +1,7 @@
+public class atv03 {
+    public static void main(String[] args) {
+        String a = "asfaf";
+        int ascii = (int) a;
+
+    }
+}
